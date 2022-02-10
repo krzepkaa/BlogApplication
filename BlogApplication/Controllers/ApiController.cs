3855e8e926ff1d2b9ab2f2f9f52b4f2939044d06
Konrad Rzepka <konrad.rzepka@microsoft.wsei.edu.pl>
2022-02-10 18:46:06 +0100

    better api connected to posts

--- BlogApplication/Controllers/ApiController.cs
+++ BlogApplication/Controllers/ApiController.cs
@@ -1,6 +1,8 @@
 ﻿using BlogApplication.Data;
+using BlogApplication.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,29 +14,94 @@ namespace BlogApplication.Controllers
     [ApiController]
     public class ApiController : ControllerBase
     {
-        private BlogDbContext _dbContext;
-        public ApiController(BlogDbContext dbContext)
+        private PostDbContext _dbContext;
+        public ApiController(PostDbContext dbContext)
         {
             _dbContext = dbContext;
         }
 
-        [HttpGet("users")]
+        [HttpGet("posts")]
         public IActionResult Get()
         {
             try
             {
-                var users = _dbContext.ApplicationUsers.ToList();
-                if(users.Count == 0)
+                var posts = _dbContext.Posts.ToList();
+                if (posts.Count == 0)
                 {
-                    return StatusCode(404, "No user found!");
+                    return StatusCode(404, "No post found!");
                 }
 
-                return Ok(users);
+                return Ok(posts);
             }
             catch (Exception)
             {
                 return StatusCode(500, "An error has occurred!");
             }
+        }
+
+        [HttpPost("createpost")]
+        public IActionResult Create([FromBody] PostModel postrequest)
+        {
+            PostModel post = new PostModel();
+            post.Title = postrequest.Title;
+            post.PostContext = postrequest.PostContext;
+            post.Author = postrequest.Author;
+            try
+            {
+                _dbContext.Posts.Add(post);
+                _dbContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "An error has occurred");
+            }
+            var posts = _dbContext.Posts.ToList();
+            return Ok(posts);
+        }
+
+        [HttpPut("updatepost")]
+        public IActionResult Update([FromBody] PostModel post)
+        {
+            try
+            {
+                var Post = _dbContext.Posts.FirstOrDefault(x => x.PostId == post.PostId);
+                if(Post == null)
+                {
+                    return (StatusCode(404, "Post not found"));
+                }
+                Post.Title = post.Title;
+                Post.PostContext = post.PostContext;
+                Post.Author = post.Author;
+                _dbContext.Entry(Post).State = EntityState.Modified;
+                _dbContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "An error has occurred");
+            }
+            var posts = _dbContext.Posts.ToList();
+            return Ok(posts);
+        }
+
+        [HttpDelete("deletepost/{postId}")]
+        public IActionResult Delete([FromRoute] PostModel post)
+        {
+            try
+            {
+                var Post = _dbContext.Posts.FirstOrDefault(x => x.PostId == post.PostId);
+                if(Post == null)
+                {
+                    return StatusCode(404, "Post not found");
+                }
+                _dbContext.Entry(Post).State = EntityState.Deleted;
+                _dbContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "An error has occurred");
+            }
+            var posts = _dbContext.Posts.ToList();
+            return Ok(posts);
         }
     }
 }
